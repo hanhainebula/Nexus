903ce0130e6fd2b877ed728b578d98a69ae6f806
PhilipGAQ <philipgaq@mail.ustc.edu.cn>
2024-12-24 14:31:57 +0800

    move callbacks

--- UniRetrieval/abc/training/embedder/AbsDataset.py
+++ UniRetrieval/abc/training/embedder/AbsDataset.py
@@ -1,22 +1,9 @@
-import os
-import math
-import random
 import logging
-import numpy as np
-import torch.distributed as dist
 from dataclasses import dataclass
 from abc import abstractmethod
-from torch.utils.data import Dataset
-from transformers import (
-    PreTrainedTokenizer, 
-    DataCollatorWithPadding,
-    TrainerCallback,
-    TrainerState,
-    TrainerControl
-)
+
 
 from UniRetrieval.abc.training.dataset import AbsDataset
-from .AbsArguments import AbsEmbedderDataArguments, AbsEmbedderTrainingArguments
 
 logger = logging.getLogger(__name__)
 
@@ -44,21 +31,4 @@ class CallbackOutput:
     save_checkpoint: str = None
     stop_training: bool = False
 
-class AbsCallback():
-    # TODO 结合 rec studio 的callback组件
-    def __init__(self, train_dataset):
-        self.train_dataset = train_dataset
-
-    @abstractmethod
-    def on_epoch_end(
-        self,
-        args: AbsEmbedderTrainingArguments,
-        state: TrainerState,
-        control: TrainerControl,
-        **kwargs
-    ):
-        """
-        Event called at the end of an epoch.
-        """
-        self.train_dataset.refresh_epoch()
         
