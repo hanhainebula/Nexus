f774b4a4ef41c0f291fe5fc01a3d0566dba8033e
PhilipGAQ <philipgaq@mail.ustc.edu.cn>
2025-01-04 16:40:03 +0800

    update onnx2trt.sh

--- examples/text_retrieval/inference/onnx2trt.sh
+++ examples/text_retrieval/inference/onnx2trt.sh
@@ -11,4 +11,8 @@ export PATH=$PATH:$TRT_PATH/bin
 
 
 # Convert ONNX to TensorRT with dynamic shapes
+# embedder:
 trtexec --onnx=$ONNX_PATH --saveEngine=$TRT_SAVE_PATH --minShapes=input_ids:1x1,attention_mask:1x1,token_type_ids:1x1 --optShapes=input_ids:8x128,attention_mask:8x128,token_type_ids:8x128 --maxShapes=input_ids:16x512,attention_mask:16x512,token_type_ids:16x512 --verbose
+
+# reranker:
+# trtexec --onnx=$ONNX_PATH --saveEngine=$TRT_SAVE_PATH --minShapes=input_ids:1x1,attention_mask:1x1 --optShapes=input_ids:8x128,attention_mask:8x128 --maxShapes=input_ids:16x512,attention_mask:16x512
