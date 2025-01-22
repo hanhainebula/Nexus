99bf88b84395728cd6552893cd885b96e1eddf77
PhilipGAQ <1592109532@qq.com>
2025-01-22 11:51:31 +0800

    adapt multinode; add passwd for redis to be connected by other host;

--- Nexus/training/reranker/recommendation/__tde_main__.py
+++ Nexus/training/reranker/recommendation/__tde_main__.py
@@ -1,6 +1,21 @@
+import os 
+import netifaces
 from Nexus.training.reranker.recommendation.tde_runner import TDERankerRunner
 from Nexus.training.reranker.recommendation.modeling import MLPRanker
 
+def get_local_interfaces():
+    local_interfaces = []
+    for interface in netifaces.interfaces():
+        addrs = netifaces.ifaddresses(interface)
+        is_lan_interface = False
+        if netifaces.AF_INET in addrs:
+            for addr in addrs[netifaces.AF_INET]:
+                ip = addr['addr']
+                if ip.startswith("172."):  # only consider LAN IP addresses
+                    is_lan_interface = True
+        if is_lan_interface:
+            local_interfaces.append(interface)
+    return local_interfaces
 
 def main():
     data_config_path = "./examples/recommendation/config/data/recflow_ranker.json"
@@ -18,4 +33,7 @@ def main():
         runner.model._id_transformer_group.__del__()
 
 if __name__ == "__main__":
+    local_interfaces = get_local_interfaces()
+    os.environ["TP_SOCKET_IFNAME"] = local_interfaces[0] # set the interface for Gloo
+    os.environ["GLOO_SOCKET_IFNAME"] = local_interfaces[0]
     main()
