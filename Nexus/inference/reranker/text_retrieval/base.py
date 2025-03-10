01ddd29c94227bea45552a361cb8eccaaf647170
PhilipGAQ <philipgaq@mail.ustc.edu.cn>
2025-03-10 19:30:20 +0800

    add license

--- Nexus/inference/reranker/text_retrieval/base.py
+++ Nexus/inference/reranker/text_retrieval/base.py
@@ -21,7 +21,29 @@ def sigmoid(x):
     x = x.item() if not isinstance(x, float) else x
     return float(1 / (1 + np.exp(-x)))
 
+# The following code is modified from FlagEmbedding, licensed under the MIT License.
+# Source: https://github.com/FlagOpen/FlagEmbedding/blob/master/FlagEmbedding/inference/reranker/encoder_only/base.py
+# Original copyright notice: 
+# MIT License
+# Copyright (c) 2022 staoxiao
 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 class BaseReranker(AbsReranker):
     """Base reranker class for encoder only models.
 
@@ -730,7 +752,6 @@ class BaseRerankerInferenceEngine(InferenceEngine):
 
             with torch.no_grad():  
                 scores = model(**encoded_inputs, return_dict=True).logits.view(-1, ).float().cpu().numpy()
-
             all_outputs.extend(scores)  
             
         if normalize:
